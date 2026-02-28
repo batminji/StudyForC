4aa7280a5c61ad8556f36d6d1e15e219e63177df
BATMINJI <minjiconan@naver.com>
2026-02-28 21:50:57 +0900

    std::memory_order_release, std::memory_order_acquire
    release 이후 명령들이 이전으로 재배치 될 수 없다.
    acquire 이전 명령들이 이후로 재배치 될 수 없다.

--- 15_3_Atomic_Memory_Order.cpp
+++ 15_3_Atomic_Memory_Order.cpp
@@ -13,6 +13,21 @@
 // => CPU에서 매우 빠른 속도로 실행할 수 있게 된다.
 // => 대신 예상치 못한 결과를 나을 수 있다.
 
+// memory_order_release
+// 해당 명령 이전의 모든 메모리 명령들이
+// 해당 명령 이후로 재배치 되는 것을 금지한다.
+// 만약에 같은 변수를 memory_order_acquired로 읽는 쓰레드가 있다면
+// memory_order_release 이전에 오는 모든 메모리 명령들이
+// 해당 쓰레드에 의해서 관찰될 수 있어야 한다.
+
+// memory_order_acquire
+// release와는 반대로,
+// 해당 명령 이후에 오는 모든 메모리 명령들이
+// 해당 명령 이전으로 재배치 되는 것을 금지한다.
+
+// => release오와 acquire를 통해서
+// 동기화를 수행하는 것을 볼 수 있다.
+
 // store(), load() 함수
 // atomic 객체들에 대해서 원자적으로 쓰기와 읽기를 지원해주는 함수
 // store() : 쓰기
@@ -35,15 +50,32 @@ void Worker2(std::atomic<int>& a, std::atomic<int>& b)
 	printf("y : %d\n", y);
 }
 
+// ------------------------------------------------------
+
+void Producer(std::atomic<bool>& bIsReady, int& Data)
+{
+	Data = 10;
+	bIsReady.store(true, std::memory_order_release);
+}
+
+void Consumer(std::atomic<bool>& bIsReady, int& Data)
+{
+	while (!bIsReady.load(std::memory_order_acquire))
+	{
+
+	}
+	printf("Data : %d\n", Data);
+}
+
 int main()
 {
 	std::vector<std::thread> Workers;
 
-	std::atomic<int> a(0);
-	std::atomic<int> b(0);
+	std::atomic<bool> bIsReady(false);
+	int Data = 0;
 
-	Workers.emplace_back(Worker1, std::ref(a), std::ref(b));
-	Workers.emplace_back(Worker2, std::ref(a), std::ref(b));
+	Workers.emplace_back(Producer, std::ref(bIsReady), std::ref(Data));
+	Workers.emplace_back(Consumer, std::ref(bIsReady), std::ref(Data));
 
 	Workers[0].join();
 	Workers[1].join();
